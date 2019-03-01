4dc22f0edb3a49ca0b4a03a9ade846eb542698da
guojiongwei <1030286235@qq.com>
2019-03-01 23:16:34 +0800

    设置https域名

--- option.js
+++ option.js
@@ -1,13 +1,28 @@
 ﻿const express = require('express');
 const app = express();
 const path = require('path');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const proxy = require('http-proxy-middleware');
 const compression = require('compression'); //解析gzip格式文件 js,css
 const history = require('connect-history-api-fallback'); //解析history路由模式项目
 app.all('*', function(req, res, next) { // 解决跨域问题
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
+  if(req.protocol == 'http') {
+    let host = req.headers.host;
+    host = host.replace(/\:\d+$/, ''); // Remove port number
+    res.redirect(`https://${host}:3000${req.originalUrl}`);
+    return
+  } else {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+  }
 });
+// Configuare https
+const httpsOption = {
+  key : fs.readFileSync(path.resolve(__dirname,"./ssl/guojiongwei.key")),
+  cert: fs.readFileSync(path.resolve(__dirname, "./ssl/guojiongwei.pem"))
+}
 app.use('/admin_demo_api', proxy({
   target: 'https://guojiongwei.com/admin_demo_api',
   pathRewrite: {'^/admin_demo_api' : '/'},
@@ -17,6 +32,10 @@ app.use(history());
 app.use(compression())
 app.use(express.static(path.join(__dirname, '/admin')));//和上面是一样的
 // 监听端口
-app.listen(4000, '0.0.0.0', () => {
- console.log(`server running localhost:4000`);
-});
+
+var httpsServer = https.createServer(httpsOption,app);
+var httpServer = http.createServer(app);
+//https监听3000端口
+httpsServer.listen(3000);
+//http监听4000端口
+httpServer.listen(4000);
